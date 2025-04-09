c44ab98df95a7ac1b1baf1e13c40449cd7305545
pimpa26 <147247522+Pimpakarn26@users.noreply.github.com>
2025-04-09 22:07:20 +0700

    Update firebase.Config.js

--- frontend/src/configs/firebase.Config.js
+++ frontend/src/configs/firebase.Config.js
@@ -7,6 +7,8 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
     appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID,
+
   };
 
   const app = initializeApp(firebaseConfig);
