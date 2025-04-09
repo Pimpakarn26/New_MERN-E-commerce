8c2e91d8d138978902c10fd85bce709e934f8dad
pimpa26 <147247522+Pimpakarn26@users.noreply.github.com>
2025-04-09 22:02:19 +0700

    Update firebase.config.js

--- Backend/config/firebase.config.js
+++ Backend/config/firebase.config.js
@@ -7,6 +7,8 @@ const firebaseConfig = {
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
+  measurementId: process.env.measurementId,
+
 };
 
 module.exports = firebaseConfig;
